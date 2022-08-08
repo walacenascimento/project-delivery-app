8eb12bda84e5e2c70d74b98ce4209c8951dbdc70
Pedro Folego <hmfpedro@hotmail.com>
2022-08-08 15:24:57 -0300

    feat: tela login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
+      <Route exact path="/" element={ <Navigate to="/login" replace /> } />
       <Route exact path="/login" element={ <Login /> } />
     </Routes>
   );
