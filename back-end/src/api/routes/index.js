e9f30f36aa477e872b0817bdf8c21c66d0433e3d
walacenascimento <wlacens@hotmail.com>
2022-08-11 20:22:36 -0300

    Implementando o back de SelelerOrders

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const user = require('./users.route');
 const product = require('./products.route');
 const admin = require('./admin.route');
+const sales = require('./sales.route');
 
 const route = Router();
 
 route.use('/login', user);
 route.use('/product', product);
 route.use('/admin/manage', admin);
+route.use('/seller/orders', sales);
 
 module.exports = route;
--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,9 +1,33 @@
-// import React, { useEffect, useState } from 'react';
-import NavbarSeller from '../components/NavBarSellers';
+import React, { useEffect, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+import NavBarSeller from '../components/NavBarSellers';
 
 function SellerOrders() {
+  const [sales, setSales] = useState([]);
+
+  useEffect(() => {
+    const getSales = async () => axios.get('localhost:3001/seller/orders');
+    console.log('entrou');
+    setSales(getSales());
+  }, []);
   return (
-    <NavbarSeller />
+    <main>
+
+      <NavBarSeller />
+      <section>
+        {sales.map(
+          ({ status, saleDate, deliveryNumber, deliveryAddress, totalPrice }, index) => (
+            <div key={ index }>
+              <h4>{status}</h4>
+              <h4>{saleDate}</h4>
+              <h4>{deliveryNumber}</h4>
+              <h4>{deliveryAddress}</h4>
+              <h4>{totalPrice}</h4>
+            </div>
+          ),
+        )}
+      </section>
+    </main>
   );
 }
 
