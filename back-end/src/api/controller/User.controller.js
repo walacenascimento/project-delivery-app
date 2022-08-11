c126635af513137fa55f122625051f983ce90943
Pedro Folego <hmfpedro@hotmail.com>
2022-08-11 16:25:40 -0300

    feat: controller createUser

--- back-end/src/api/controller/User.controller.js
+++ back-end/src/api/controller/User.controller.js
@@ -1,15 +1,22 @@
 const { statusMessage } = require('../utils/functions');
-const { validateLogin, createToken } = require('../services/User.service');
+const service = require('../services/User.service');
 
 const validateLoginController = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await validateLogin(email, password);
+  const user = await service.validateLogin(email, password);
   if (!user) return next(statusMessage(404, 'Not found'));
 
-  const token = createToken(user);
+  const token = service.createToken(user);
   return res.status(200).json({ ...user, token });
+};
+
+const createUser = async (req, res, _next) => {
+  const { email, password, name } = req.body;
+  await service.createUser(name, email, password, 'customer');
+  return res.status(201).json();
 };
 
 module.exports = {
   validateLoginController,
+  createUser,
 };
