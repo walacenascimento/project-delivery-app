8d1e767e11db87290b46482f958ffaa799d73794
Pedro Folego <hmfpedro@hotmail.com>
2022-08-11 19:05:58 -0300

    refator: criando um forms pra poder usar o data-testid

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,8 +1,14 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import FormRegister from '../components/FormRegister';
 
 function Register() {
-  dataTestIds = {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validRegister, setValidRegister] = useState(true);
+  const [invalidRegister, setInvalidRegister] = useState(false);
+
+  const dataTestIds = {
     name: 'common_register__input-name',
     email: 'common_register__input-email',
     password: 'common_register__input-password',
@@ -10,7 +16,27 @@ function Register() {
     invalidFields: 'common_register__element-invalid_register',
   };
 
-  onSubmitRegister = async (name, email, password) => {
+  useEffect(() => {
+    const validateFields = () => {
+      const minNameLength = 11;
+      const regexEmail = /\S+@\S+\.\S+/;
+      const minPasswordLength = 5;
+      if (name.length > minNameLength && regexEmail
+        .test(email) && password.length > minPasswordLength) {
+        return setValidRegister(false);
+      }
+      setValidRegister(true);
+    };
+
+    validateFields();
+  }, [email, password, name]);
+
+  const handleChange = (event, setAttr) => {
+    setInvalidRegister(false);
+    setAttr(event.target.value);
+  };
+
+  const onSubmitRegister = async () => {
     await axios.post('http://localhost:3001/register', {
       name,
       email,
@@ -21,7 +47,55 @@ function Register() {
   return (
     <main>
       <h1>Register</h1>
-      <FormRegister dataTestIds={ dataTestIds } />
+      <form onSubmit={ (event) => event.preventDefault() }>
+        <h2>Cadastrar novo usuário</h2>
+        <label htmlFor="Input-name">
+          <input
+            className="cadastro-nome"
+            value={ name }
+            onChange={ (e) => handleChange(e, setName) }
+            placeholder="Nome e sobrenome"
+            data-testid={ dataTestIds.name }
+          />
+        </label>
+        <label htmlFor="Input-email">
+          <input
+            className="cadastro-email"
+            type="email"
+            value={ email }
+            onChange={ (e) => handleChange(e, setEmail) }
+            placeholder="seu-email@site.com.br"
+            data-testid={ dataTestIds.email }
+          />
+        </label>
+        <label htmlFor="Input-password">
+          <input
+            className="cadastro-password"
+            type="password"
+            value={ password }
+            onChange={ (e) => handleChange(e, setPassword) }
+            placeholder="*********"
+            data-testid={ dataTestIds.password }
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={ onSubmitRegister }
+          disabled={ validRegister }
+          data-testid={ dataTestIds.button }
+        >
+          CADASTRAR
+        </button>
+        { invalidRegister
+      && (
+        <h2
+          data-testid={ dataTestIds.invalidFields }
+        >
+          Dados inválidos
+
+        </h2>
+      ) }
+      </form>
     </main>
   );
 }
