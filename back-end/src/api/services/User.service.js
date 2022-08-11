0debe91ef5ab76959aea57da66178fb28b5e6344
Pedro Folego <hmfpedro@hotmail.com>
2022-08-11 15:00:03 -0300

    fix: user service

--- back-end/src/api/services/User.service.js
+++ back-end/src/api/services/User.service.js
@@ -5,13 +5,14 @@ const { User } = require('../../database/models');
 async function validateLogin(emailLogin, password) {
   const convertedPassword = md5(password).toString();
   const user = await User.findOne({ where: { email: emailLogin, password: convertedPassword } });
+  if (!user) return null;
 
   const { email, name, role } = user;
   return { email, name, role };
 }
 
 function createToken(user) {
-  const token = jwt.sign({ ...user }, 'password');
+  const token = jwt.sign({ ...user }, 'secret_key');
   return token;
 }
 
