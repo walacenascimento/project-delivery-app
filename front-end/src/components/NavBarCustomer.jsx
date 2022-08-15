ac184ce828e9c8822fba8a43ec794e175ada8871
Pedro Folego <hmfpedro@hotmail.com>
2022-08-15 18:36:34 -0300

    feat: add navbar and data-testid productdetail

--- front-end/src/components/NavBarCustomer.jsx
+++ front-end/src/components/NavBarCustomer.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function NavBarCustomer(props) {
@@ -7,10 +7,26 @@ function NavBarCustomer(props) {
 
   return (
     <header>
-      <h2>Meus Pedidos</h2>
-      <h2>Produtos</h2>
-      <h2>{user}</h2>
-      <button type="button" onClick={ () => navigate('/') }>
+      <Link
+        to="/customer/orders"
+        data-testid="customer_products__element-navbar-link-orders"
+      >
+        Meus Pedidos
+
+      </Link>
+      <Link
+        to="/customer/products"
+        data-testid="customer_products__element-navbar-link-products"
+      >
+        Produtos
+
+      </Link>
+      <h2 data-testid="customer_products__element-navbar-user-full-name">{user}</h2>
+      <button
+        data-testid="customer_products__element-navbar-link-logout"
+        type="button"
+        onClick={ () => navigate('/') }
+      >
         Sair
       </button>
     </header>
--- front-end/src/pages/ProductDetail.jsx
+++ front-end/src/pages/ProductDetail.jsx
@@ -1,17 +1,96 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import NavBarCustomer from '../components/NavBarCustomer';
+import { getLocalStorage } from '../services/localStorage';
 
 function ProductDetail() {
-  const [order, setOrder] = useState({});
+  const [{ saleOrder, sales }, setOrder] = useState({ saleOrder: {}, sales: [] });
+  const nameUser = getLocalStorage('user').name;
   useEffect(() => {
     const noMagicNumber = 17;
     const id = window.location.pathname.slice(noMagicNumber);
-    const getProducts = async () => axios.get(`http://localhost:3001/sales/${id}`);
-    setOrder(getProducts().data);
+
+    axios
+      .get(`http://localhost:3001/sales/${id}`)
+      .then((response) => setOrder(response.data));
   }, []);
   return (
     <main>
-      <h2>{order.name}</h2>
+      <NavBarCustomer user={ nameUser } />
+      {console.log(saleOrder, sales)}
+      <h2
+        data-testid="customer_order_details__element-order-details-label-order-id"
+      >
+        {saleOrder.id}
+
+      </h2>
+      <h2
+        data-testid="customer_order_details__element-order-details-label-seller-name"
+      >
+        {nameUser}
+
+      </h2>
+      <h2
+        data-testid="customer_order_details__element-order-details-label-order-date"
+      >
+        {saleOrder.saleDate}
+
+      </h2>
+      <h2
+        data-testid="customer_order_details__element-order-details-label-delivery-status"
+      >
+        {saleOrder.status}
+
+      </h2>
+      <h2
+        data-testid="customer_order_details__element-order-total-price"
+      >
+        {saleOrder.totalPrice}
+
+      </h2>
+      {sales.map((s, index) => (
+        <div key={ s.id }>
+
+          <h5
+            data-testid={
+              `customer_order_details__element-order-table-item-number-${index}`
+            }
+          >
+            {index}
+
+          </h5>
+          <h5
+            data-testid={ `customer_order_details__element-order-table-name-${index}` }
+          >
+            {s.product.name}
+
+          </h5>
+          <p
+            data-testid={
+              `customer_order_details__element-order-table-quantity-${index}`
+            }
+          >
+            {s.quantity}
+
+          </p>
+          <p
+            data-testid={
+              `customer_order_details__element-order-table-unit-price-${index}`
+            }
+          >
+            {s.product.price}
+
+          </p>
+          <p
+            data-testid={
+              `customer_order_details__element-order-table-sub-total-${index}`
+            }
+          >
+            {Number(s.product.price) * Number(quantity)}
+
+          </p>
+        </div>
+      ))}
     </main>
   );
 }
